14228abe5392e0ed156e207d639e70489ced8072
Vitalik <vitalijzaxarov99@mail.ru>
2023-06-24 17:26:48 +0300

    семинар 8

--- Task48/Seminar8.py
+++ Task48/Seminar8.py
@@ -17,43 +17,143 @@
 # (*) Усложнение.
 # Сделать тесты для функций
 # Разделить на model-view-controller
-def input_User() -> list:
-    user=[]
-    user.append(input("Input first name"))
-    user.append(input("Input second name"))
-    user.append(input("Input disription"))
+# def input_User() -> list:
+#     user=[]
+#     user.append(input("Input first name"))
+#     user.append(input("Input second name"))
+#     user.append(input("Input disription"))
 
-    return user
-# print (input_User())
-key_count = 0
-phone_dir = dict()
-def create( phone_dir_local: dict, idc: int, user:list) ->dict:
-    idc += 1
-    phone_dir_local[idc]= user
-    return phone_dir_local, idc
+#     return user
+# # print (input_User())
+# key_count = 0
+# phone_dir = dict()
+# def create( phone_dir_local: dict, idc: int, user:list) ->dict:
+#     idc += 1
+#     phone_dir_local[idc]= user
+#     return phone_dir_local, idc
 
 # user1 = ["second_name1", "first_name1","phone1","discription1"]
 # user2 = ["second_name2", "first_name2","phone2","discription2"]
 
 # phone_dir, key_count=create(phone_dir,key_count,user1)
 # phone_dir, key_count=create(phone_dir,key_count,user2)
-# print(phone_dir)
+# # print(phone_dir)
 
-def menu():
-    print("Введите 1, если хотите ввести пользователя ")
-    print("Введите 2, если хотите распечатать справочник ")
-    print("Введите 3 для экспорта")
-    key_count = 0
-    phone_dir = dict()
-    while True:
-        num = int(input("Выберите операцию "))
-        if num == 0:
-            break
-        if(num==1):
-            user = input_User()
-            phone_dir, key_count = create(phone_dir, key_count, user)
-            if num == 2:
-                print(phone_dir)
-            if num == 3:
-                file_name = input("Выберите имя файла ")    
-menu()               
+# def menu():
+#     print("Введите 1, если хотите ввести пользователя ")
+#     print("Введите 2, если хотите распечатать справочник ")
+#     print("Введите 3 для экспорта")
+#     key_count = 0
+#     phone_dir = dict()
+#     while True:
+#         num = int(input("Выберите операцию "))
+#         if num == 0:
+#             break
+#         if(num==1):
+#             user = input_User()
+#             phone_dir, key_count = create(phone_dir, key_count, user)
+#             if num == 2:
+#                 print(phone_dir)
+#             if num == 3:
+#                 file_name = input("Выберите имя файла ")    
+
+# def export_phone_dir(phone_dir: dict, file_name: str):
+#     MAIN_DIR = abspath(dirname(__file__))
+#     full_name = join(MAIN_DIR, file_name +'.txt')
+#     with open(full_name, mode='w', encoding='utf-a') as file:
+#         for idc, user in phone_dir.items():
+#             file.write(f"{idc}:{user[0]},{user[1]}.{user[2]},{user[3]}\n")
+
+# from os.path import join, abspath, dirname
+
+
+# # menu()      
+# export_phone_dir(phone_dir, "phones")         
+
+def show_data(filename):
+    print("\nПП | ФИО | Телефон")
+    with open(filename, "r", encoding="utf-8") as data:
+        print(data.read())
+    print("")
+
+# Записывает информацию в файл
+def export_data(filename):
+    with open(filename, "r", encoding="utf-8") as data:
+        tel_file = data.read()
+    num = len(tel_file.split("\n"))
+    with open(filename, "a", encoding="utf-8") as data: 
+        fio = input("Введите ФИО: ")
+        phone_number = input("Введите номер телефона: ")
+        data.write(f"{num} | {fio} | {phone_number}\n")
+        print(f"Добавлена запись : {num} | {fio} | {phone_number}\n")
+
+# Изменяет информацию из файла
+def edit_data(filename):
+    print("\nПП | ФИО | Телефон")
+    with open(filename, "r", encoding='utf-8') as data:
+        tel_book = data.read()
+    print(tel_book)
+    print("")
+    index_delete_data = int(input("Введите номер строки для редактирования: ")) - 1
+    tel_book_lines = tel_book.split("\n")
+    edit_tel_book_lines = tel_book_lines[index_delete_data]
+    elements = edit_tel_book_lines.split(" | ")
+    fio = input("Введите ФИО: ")
+    phone = input("Введите номер телефона: ")
+    num = elements[0]
+    if len(fio) == 0:
+        fio = elements[1]
+    if len(phone) == 0:
+        phone = elements[2]
+    edited_line = f"{num} | {fio} | {phone}"
+    tel_book_lines[index_delete_data] = edited_line
+    print(f"Запись - {edit_tel_book_lines}, изменена на - {edited_line}\n")
+    with open(filename, "w", encoding='utf-8') as f:
+        f.write("\n".join(tel_book_lines))
+
+# Удаляет информацию из файла
+def delete_data(filename):
+    print("\nПП | ФИО | Телефон")
+    with open(filename, "r", encoding="utf-8") as data:
+        tel_book = data.read()
+        print(tel_book)
+    print("")
+    index_delete_data = int(input("Введите номер строки для удаления: ")) - 1
+    tel_book_lines = tel_book.split("\n")
+    del_tel_book_lines = tel_book_lines[index_delete_data]
+    tel_book_lines.pop(index_delete_data)
+    print(f"Удалена запись: {del_tel_book_lines}\n")
+    with open(filename, "w", encoding='utf-8') as data:
+        data.write("\n".join(tel_book_lines))
+
+def main():
+    my_choice = -1
+    file_tel = "tel.txt"
+
+    # Создает файл если его нет в папке
+    with open(file_tel, "a", encoding="utf-8") as file:
+         file.write("")
+
+    while my_choice != 0:
+        print("Выберите одно из действий:")
+        print("1 - Вывести инфо на экран")
+        print("2 - Произвести экпорт данных")
+        print("3 - Произвести изменение данных")
+        print("4 - Произвести удаление данных")
+        print("0 - Выход из программы")
+        action = int(input("Действие: "))
+        if action == 1:
+            show_data(file_tel)
+        elif action == 2:
+            export_data(file_tel)
+        elif action == 3:
+            edit_data(file_tel)
+        elif action == 4:
+            delete_data(file_tel)
+        else:
+            my_choice = 0
+
+    print("До свидания")
+
+if __name__ == "__main__":
+    main()
